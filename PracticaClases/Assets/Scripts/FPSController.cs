d1f886c02e45779bcc5822413e3e5837b4cfe2d4
Marcos Sosa} <marquitos.sosa69@gmail.com>
2025-03-26 18:53:34 -0300

    Updated FPS Controller - Agregado Movimiento Camra

--- PracticaClases/Assets/Scripts/FPSController.cs
+++ PracticaClases/Assets/Scripts/FPSController.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
+    [Header("Movimiento Personaje")]
     public float speed = 5f;
     public CharacterController controller;
+
+
+    [Header("Movimiento de Camara")]
+    public Transform camTransform;
+    public float mouseSensitivityX = 2f;
+    public float mouseSensitivityY = 2f;
+    private float verticalRotation = 0f;
 
     void Update()
     {
         MoverJugador();
+        MoverCamara();
     }
 
     void MoverJugador() 
@@ -17,8 +26,20 @@ public class FPSController : MonoBehaviour
 
         Vector3 movimiento = transform.right * movX + transform.forward * movY;
 
-        controller.Move(movimiento * speed * Time.deltaTime);
-       
+        controller.Move(movimiento * speed * Time.deltaTime);       
+    }
+
+    void MoverCamara()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        camTransform.localRotation = Quaternion.Euler(verticalRotation,0,0f);
+        transform.Rotate(Vector3.up * mouseX);      
+         
     }
 
 
